230657837794ba60ebe16eef5ee5f129f33e18d3
Sulaiman YUSOF <syusof@e3r8p1.42.fr>
2016-09-22 13:23:01 +0200

    r

--- ft_proceed.c
+++ ft_proceed.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/29 15:02:24 by syusof            #+#    #+#             */
-/*   Updated: 2016/09/22 13:03:59 by syusof           ###   ########.fr       */
+/*   Updated: 2016/09/22 13:22:23 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@ void ft_proceed(t_node *node1, t_lst2 *lstprime, t_lst2 *lstremain,char *strbegi
 {
 	
 	t_path		*path1;
+	t_node		*nodetmp;
 
 	path1 = NULL;
-	path1 = ft_createlst(strbegi);
-	while(ft_strcmp(node1->name,strend) != 0)
+	nodetmp = node1;
+	path1 = ft_createlstpath(strbegi);
+	while(ft_strcmp(nodetmp->name,strend) != 0)
 	{
-		
+		while(ft_strcmp(nodetmp->name,strend) != 0)
+		{
+			nodetmp = nodetmp->nextl;
+		}
 	}
 }
