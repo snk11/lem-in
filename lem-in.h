f1a3b8595dc318a112de9f7ebe5502d6c3ce264d
Sulaiman YUSOF <syusof@e3r8p1.42.fr>
2016-10-06 09:33:43 +0200

    r

--- lem-in.h
+++ lem-in.h
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/29 14:39:56 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/06 09:24:30 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/06 09:31:12 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,6 @@ typedef struct			s_node
 
 t_lst		*create_lst1(void *content);
 t_lst2		*create_lst2(void *content);
-int		ft_checkremain(t_lst *lstremain, char *s);
-void	ft_remove_if(t_lst2 **lsta, t_lst2 *lstb);
 t_node		*ft_createnode(char *s);
 //t_path		*ft_createlstpath(char *s);
 //t_path		*lst_add_down(t_path *path1);
@@ -43,21 +41,14 @@ t_node		*lst_add_downl(t_node **node1,char *s);
 t_node		*lst_add_downr(t_node **node1,t_node *noderef,char *s);
 t_node		*lst_add_down_downr(t_node **node1,char *s);
 t_node		*lst_add_downlrnew(t_node **node1,char *strbegi);
-t_node		*lst_add_downlrnew_minus(t_node **node1, int p,char *strbegi);
 int		ft_checknode(t_node *path1,char *s);
 int		ft_checknode2(t_node *node,char *s);
-void	ft_search1(t_node *node1,t_node *noderef,char *strend,t_node **path1begi, int nbelem,char *strbegi,int *ind1);
-void ft_proceed(t_node *node1,t_node **processnodes,t_node **lvnodes,char *strbegi,char *strend);
-int		ft_checkp(t_node *node1,int p);
-int		ft_checknew(t_node *path1);
 void	ft_printfpath(t_node *path1begi);
 t_node *ft_copytree(t_node *maintree,char *strbegi);
 t_node	*ft_maketree(t_node *maintree,char *strbegi);
 int		ft_countelem_lv1(t_node *node1);
-void	ft_solv(t_node *node1,t_node *noderef,char *strend,t_node **path1begi,char *strbegi,int *ind1);
 t_node *ft_cursref(t_node *node1,t_node *noderef);
 t_node *ft_cursref_first(t_node *node1,char *s);
-t_node		*lst_rmtop(t_node **node1);
 void permute(char *a, int l, int r);
 void swap(char *x, char *y);
 t_node	*ft_maketree2(t_node *maintree,char *strbegi);
