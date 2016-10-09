eea99a66d6b57ee173dd1d89cd0e193ec15fe6dc
Sulaiman YUSOF <syusof@e3r8p1.42.fr>
2016-10-09 13:28:40 +0200

    r

--- ft_screenbest.c
+++ ft_screenbest.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/06 09:54:27 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/06 12:11:28 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/09 13:28:28 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,90 +18,71 @@
 t_node	*ft_screenbest(t_node **nodebest,t_node *nodedata,char *strend)
 {
 	int	min;
+	int	minbest;
+	int	minbest2;
 	int cnt;
 	int r;
+	int	iter;
+	int nbelemnodedata;
 	t_node	*nodetmp;
 	t_node	*nodetmp3;
 
 	nodetmp = NULL;
 	nodetmp3= NULL;
 	min = 0;
+	minbest = 0;
+	minbest2 = 0;
+	iter = 0;
+	nbelemnodedata = 0;
 	cnt = 0;
 	r = 0;
+	nbelemnodedata = ft_countelem_lv1(nodedata);
 	nodetmp = nodedata;
 	nodetmp3 = nodetmp;
-	while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0)
+	while(iter <= nbelemnodedata)
 	{
-		cnt++;
-		nodetmp = nodetmp->nextr;
-	}
-	min = cnt;
-	nodetmp = nodetmp3;
-	nodetmp = nodetmp->nextl;
-	while(nodetmp)
-	{
+		nodetmp = nodedata;
 		nodetmp3 = nodetmp;
+		min = 0;
 		cnt = 0;
 		while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0)
 		{
 			cnt++;
 			nodetmp = nodetmp->nextr;
 		}
-		if(cnt < min)
+		if(cnt > minbest)
 			min = cnt;
 		nodetmp = nodetmp3;
 		nodetmp = nodetmp->nextl;
-	}
-
-	nodetmp = nodedata;
-	cnt = 0;
-	while(nodetmp && cnt != min)
-	{
-		nodetmp3 = nodetmp;
-		cnt = 0;
-		while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0)
+		while(nodetmp)
 		{
-			cnt++;
-			nodetmp = nodetmp->nextr;
-		}
-		nodetmp = nodetmp3;
-		if(cnt == min)
-		{
-			*nodebest = lst_add_downlrnew(nodebest,nodetmp->name);
-			nodetmp = nodetmp->nextr;
-			while(nodetmp)
+			nodetmp3 = nodetmp;
+			cnt = 0;
+			while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0)
 			{
-				*nodebest = lst_add_down_downr(nodebest,nodetmp->name);
+				cnt++;
 				nodetmp = nodetmp->nextr;
 			}
+			if(min == 0 && cnt > minbest)
+				min = cnt;
+			if(cnt > minbest && cnt < min)
+					min = cnt;
+			nodetmp = nodetmp3;
+			nodetmp = nodetmp->nextl;
 		}
-		nodetmp = nodetmp3;
-		nodetmp = nodetmp->nextl;
-	}
-
-	nodetmp = nodedata;
-	cnt = 0;
-	while(nodetmp)
-	{
-		nodetmp3 = nodetmp;
+		nodetmp = nodedata;
 		cnt = 0;
-		r = 0;
-		while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0)
+		while(nodetmp && min > 0 && cnt != min)
 		{
-			cnt++;
-			nodetmp = nodetmp->nextr;
-		}
-		nodetmp = nodetmp3;
-		if(cnt == min)
-		{
-			while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0 && r == 0)
+			nodetmp3 = nodetmp;
+			cnt = 0;
+			while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0)
 			{
-				if(ft_checknode3(*nodebest,strend,nodetmp->name) == 0)
-					r = 1;
+				cnt++;
 				nodetmp = nodetmp->nextr;
 			}
 			nodetmp = nodetmp3;
-			if(r == 0)
+			if(cnt == min)
 			{
 				*nodebest = lst_add_downlrnew(nodebest,nodetmp->name);
 				nodetmp = nodetmp->nextr;
@@ -111,9 +92,49 @@ t_node	*ft_screenbest(t_node **nodebest,t_node *nodedata,char *strend)
 					nodetmp = nodetmp->nextr;
 				}
 			}
+			nodetmp = nodetmp3;
+			nodetmp = nodetmp->nextl;
 		}
-		nodetmp = nodetmp3;
-		nodetmp = nodetmp->nextl;
+
+		nodetmp = nodedata;
+		cnt = 0;
+		while(nodetmp && min > 0)
+		{
+			nodetmp3 = nodetmp;
+			cnt = 0;
+			r = 0;
+			while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0)
+			{
+				cnt++;
+				nodetmp = nodetmp->nextr;
+			}
+			nodetmp = nodetmp3;
+			if(cnt == min)
+			{
+				while(nodetmp && ft_strcmp(nodetmp->name,strend) != 0 && r == 0)
+				{
+					if(ft_checknode3(*nodebest,strend,nodetmp->name) == 0)
+						r = 1;
+					nodetmp = nodetmp->nextr;
+				}
+				nodetmp = nodetmp3;
+				if(r == 0)
+				{
+					*nodebest = lst_add_downlrnew(nodebest,nodetmp->name);
+					nodetmp = nodetmp->nextr;
+					while(nodetmp)
+					{
+						*nodebest = lst_add_down_downr(nodebest,nodetmp->name);
+						nodetmp = nodetmp->nextr;
+					}
+				}
+			}
+			nodetmp = nodetmp3;
+			nodetmp = nodetmp->nextl;
+		}
+		if (minbest < min)
+			minbest = min;
+		iter++;
 	}
 	return (*nodebest);
 }
--- lem-in.c
+++ lem-in.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/29 14:39:42 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/06 17:27:08 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/09 11:24:16 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "lem-in.h"
@@ -15,81 +15,6 @@
 
 
 #include <stdio.h>
-/*
-   int main(int ac,char **av)
-   {
-   t_node	*node1;
-   t_node	*nodetmp;
-   t_node	*path1begi;
-   de
-   t_node	*path1;
-   t_node	*path1ind;
-
-   path1begi = NULL;
-   path1 = NULL;
-   path1ind = NULL;
-   nodetmp = node1;
-   node1 = ft_createnode("0");
-   node1 = lst_add_downl(&node1,"1");
-   node1 = lst_add_downl(&node1,"2");
-   node1 = lst_add_downl(&node1,"3");
-
-   nodetmp = node1;
-   if(nodetmp)
-   {
-   printf("createR 1\n");
-   node1 = lst_add_downr(&node1,nodetmp,"1");
-   }
-
-   nodetmp = node1;
-   nodetmp = nodetmp->nextl;
-   if(nodetmp)
-   {
-   printf("createR 0\n");
-   lst_add_downr(&node1,nodetmp,"0");
-   printf("createR 2\n");
-   lst_add_downr(&node1,nodetmp,"2");
-   }
-
-   nodetmp = node1;
-   nodetmp = nodetmp->nextl;
-   nodetmp = nodetmp->nextl;
-   if(nodetmp)
-   {
-   printf("createR 1\n");
-   lst_add_downr(&node1,nodetmp,"1");
-   printf("createR 3\n");
-   lst_add_downr(&node1,nodetmp,"3");
-   }
-
-   nodetmp = node1;
-   nodetmp = nodetmp->nextl;
-   nodetmp = nodetmp->nextl;
-   nodetmp = nodetmp->nextl;
-   if(nodetmp)
-   {
-   printf("createR 2\n");
-   lst_add_downr(&node1,nodetmp,"2");
-   }
-
-   ft_proceed(node1,&path1begi,"0","3");
-
-   while(path1begi)
-   {
-   path1ind = path1begi;
-   while (path1begi)
-   {
-   printf("path = %s\n",path1begi->name);
-   path1begi = path1begi->nextr;
-   }
-   printf("\n");
-   path1begi = path1ind;
-   path1begi = path1begi->nextl;
-   }
-}
-*/
-
-
 int main(int ac,char **av)
 {
 	t_node	*node1;
