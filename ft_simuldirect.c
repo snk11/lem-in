51735d296457ba1ed34e904264b55a64c4bd5be0
Sulaiman YUSOF <syusof@e1r7p17.42.fr>
2016-12-20 18:38:13 +0100

    r

--- ft_simuldirect.c
+++ ft_simuldirect.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 17:13:07 by syusof            #+#    #+#             */
-/*   Updated: 2016/12/20 18:29:42 by syusof           ###   ########.fr       */
+/*   Updated: 2016/12/20 18:37:53 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ void	ft_simuldirect(t_node *nodebest,char *strbegi,char *strend,int totant)
 	t_simul		simul1;
 
 	ft_init7(&simul1);
-	simul1.rooms = ft_createroom(&(simul1.rooms),nodebest,strend);
-	simul1.nbtmp = totant;
-	simul1.nodetmp = simul1.rooms;
+	ft_simuldirect_p0(&simul1, nodebest, strend, totant);
 	while(simul1.r1 == 0)
 	{
 		simul1.nodetmp = simul1.rooms;
@@ -43,6 +41,13 @@ void	ft_simuldirect(t_node *nodebest,char *strbegi,char *strend,int totant)
 		ft_addseries(&(simul1.node2),simul1.node1);
 	}
 	ft_printsol(simul1.node2);
+}
+
+void		ft_simuldirect_p0(t_simul *simul1, t_node *nodebest, char *strend, int totant)
+{
+	simul1->rooms = ft_createroom(&(simul1->rooms),nodebest,strend);
+	simul1->nbtmp = totant;
+	simul1->nodetmp = simul1->rooms;
 }
 
 void		ft_simuldirect_p1(t_simul *simul1, int totant)
@@ -103,62 +108,4 @@ void		ft_simuldirect_p2(t_simul *simul1, char *strend, int totant)
 	}
 }
 
-void		ft_simuldirect_p2_p0(t_simul *simul1)
-{
-	while(simul1->nodetmp && simul1->nodetmp->nextr)
-		simul1->nodetmp = simul1->nodetmp->nextr;
-	simul1->nodetmp5 = simul1->nodetmp;
-	simul1->nodetmp4 = simul1->nodetmp;
-}
 
-
-void		ft_simuldirect_p2_p1(t_simul *simul1, char *strend)
-{
-		while(simul1->nodetmp && simul1->nodetmp->nextr && simul1->nodetmp != simul1->nodetmp5 && ft_strcmp((simul1->nodetmp->nextr)->name,strend) != 0)
-		{
-			simul1->nodetmp4 = simul1->nodetmp;
-			simul1->nodetmp = simul1->nodetmp->nextr;
-		}
-}
-
-
-void		ft_simuldirect_p2_p2(t_simul *simul1, char *strend, int totant)
-{
-			if (simul1->nodetmp->client != 0)
-			{
-				simul1->s1 = ft_makestring(simul1->nodetmp->client, strend);
-				simul1->node1 = lst_add_down_downr(&(simul1->node1),simul1->s1);
-				simul1->nodetmp->client = 0;
-			}
-			if(simul1->nbtmp > 0 && simul1->nodetmp == simul1->nodetmp3 && simul1->nodetmp->client == 0 && ft_checknewant(simul1->rooms,simul1->nodetmp3,simul1->nbtmp))
-			{
-				simul1->s1 = ft_makestring(totant - simul1->nbtmp + 1,simul1->nodetmp->name);
-				simul1->node1 = lst_add_down_downr(&(simul1->node1),simul1->s1);
-				simul1->nodetmp->client = totant - simul1->nbtmp + 1;
-				simul1->nbtmp--;
-			}
-			else if(simul1->nodetmp != simul1->nodetmp3)
-			{
-				simul1->nodetmp->client = simul1->nodetmp4->client;
-				simul1->nodetmp4->client = 0;
-				simul1->nodetmp5 = simul1->nodetmp;
-			}
-			simul1->nodetmp5 = simul1->nodetmp4;
-}
-
-void		ft_simuldirect_p2_p3(t_simul *simul1)
-{
-			simul1->s1 = ft_makestring(simul1->nodetmp4->client,simul1->nodetmp->name);
-			simul1->node1 = lst_add_down_downr(&(simul1->node1),simul1->s1);
-			simul1->nodetmp->client = simul1->nodetmp4->client;
-			simul1->nodetmp4->client = 0;
-			simul1->nodetmp5 = simul1->nodetmp;
-}
-
-void		ft_simuldirect_p2_p4(t_simul *simul1, int totant)
-{
-			simul1->s1 = ft_makestring(totant - simul1->nbtmp + 1,simul1->nodetmp->name);
-			simul1->node1 = lst_add_down_downr(&(simul1->node1),simul1->s1);
-			simul1->nodetmp->client = totant - simul1->nbtmp + 1;
-			simul1->nbtmp--;
-}
