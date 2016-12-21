f43c9c684bcda22c2e2ed8019bcae0b816d07760
Sulaiman YUSOF <syusof@e3r10p2.42.fr>
2016-12-21 17:46:44 +0100

    r

--- function.c
+++ function.c
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/31 18:32:04 by syusof            #+#    #+#             */
-/*   Updated: 2016/12/15 14:34:17 by syusof           ###   ########.fr       */
+/*   Updated: 2016/12/21 17:46:03 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,7 @@
 
 
 #include <stdio.h>
-/*
-t_node		*lst_rmnoderef(t_node **node1,t_node *noderef)
-{
-	t_node	*nodetmp;
-	t_node	*nodetmp4;
 
-	nodetmp = NULL;
-	nodetmp4 = NULL;
-	nodetmp = *node1;
-	while(nodetmp && nodetmp != noderef)
-	{
-		nodetmp4 = nodetmp;
-		nodetmp = nodetmp->nextl;
-	}
-	if(nodetmp == *node1)
-		*node1 = lst_rmtop(node1);
-	else if(nodetmp)
-	{
-		nodetmp4->nextl = nodetmp->nextl;
-	}
-	return (*node1);
-}
-
-t_node		*lst_rmtop(t_node **node1)
-{
-	if(*node1)
-	{
-		*node1 = (*node1)->nextl;
-	}
-	return (*node1);
-}
-*/
 void		ft_lstop_add_down(t_lst **toplist, char *s)
 {
 	t_op	*e;
@@ -56,7 +25,6 @@ void		ft_lstop_add_down(t_lst **toplist, char *s)
 	i = 0;
 	lstmp = NULL;
 	lstmp2 = *toplist;
-	e = NULL;
 	e = (t_op*)malloc(sizeof(t_op));
 	e->val = (char*)malloc(sizeof(char)*ft_strlen(s)+1);
 	while(*s)
@@ -66,14 +34,8 @@ void		ft_lstop_add_down(t_lst **toplist, char *s)
 		i++;
 	}
 		lstmp = create_lst(e);
-		free(e);
-		e = NULL;
-
 	if (*toplist == NULL)
-	{
-//		ft_freelst(toplist);
 		*toplist = lstmp;
-	}
 	else
 	{
 		while(lstmp2 && lstmp2->next)
@@ -253,57 +215,6 @@ t_room		*lst_add_down_downr_room(t_room **node1,char *s)
 	(nodetmp2)->nextr = nodetmp;
 	return (*node1);
 }
-/*
-t_node		*lst_add_downlrnew_minus(t_node **node1, int p,char *strbegi)
-{
-	printf("newminus\n");
-	t_node	*nodetmp;
-	t_node	*nodetmp2;
-	t_node	*nodetmp3;
-	t_node	*nodetmp4;
-	int		ind1;
-	int cnt1;
-
-	ind1 = 0;
-	cnt1 = 0;
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp4 = NULL;
-	if(*node1 == NULL)
-	{
-		*node1 = lst_add_downl(node1,strbegi);
-		return (*node1);
-	}
-	while(nodetmp2 && nodetmp2->nextl)
-	{
-		nodetmp2 = (nodetmp2)->nextl;
-		cnt1++;
-	}
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp4 = NULL;
-	while(nodetmp2 && nodetmp2->nextl)
-		nodetmp2 = (nodetmp2)->nextl;
-	nodetmp = nodetmp2->nextl;
-	nodetmp3 = nodetmp;
-	*node1 = lst_add_downl(node1,strbegi);
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp4 = NULL;
-	while(cnt1 > 0)
-	{
-		nodetmp2 = (nodetmp2)->nextl;
-		cnt1--;
-	}
-	nodetmp2 = nodetmp2->nextr;
-	while(nodetmp2 && nodetmp2->nextr)
-	{
-		*node1 = lst_add_down_downr(node1,nodetmp2->name);
-		nodetmp2 = nodetmp2->nextr;
-	}
-	return (*node1);
-}
-*/
 
 t_node		*ft_createnode(char *s)
 {
@@ -326,43 +237,6 @@ t_node		*ft_createnode(char *s)
 	curlst->nextr = NULL;
 	return (curlst);
 }
-
-/*
-t_path		*ft_createlstpath(char *s)
-{
-	t_path	*curlst;
-
-	curlst = NULL;
-	if (!(curlst = (t_path*)malloc(sizeof(t_path))))
-		return (NULL);
-	if (s == NULL)
-	{
-		curlst->name = NULL;
-	}
-	else
-	{
-		if (!(curlst->name = malloc(sizeof(s))))
-			return (NULL);
-		curlst->name = ft_memmove(curlst->name, s, sizeof(s));
-	}
-	curlst->next = NULL;
-	return (curlst);
-}
-
-t_path		*lst_add_down(t_path *path1)
-{
-	t_path	*pathtmp;
-
-	pathtmp = NULL;
-	pathtmp = ft_createlstpath(path1->name);
-	if (!path1)
-		return (pathtmp);
-	while (path1)
-		path1 = (path1)->next;
-	path1->next = pathtmp;
-	return (pathtmp);
-}
-*/
 
 t_node		*lst_add_downl(t_node **node1,char *s)
 {
@@ -431,47 +305,3 @@ t_node		*lst_add_down_downr(t_node **node1,char *s)
 	(nodetmp2)->nextr = nodetmp;
 	return (*node1);
 }
-
-/*
-t_lst		*create_lst1(void *content)
-{
-	t_lst	*curlst;
-
-	curlst = NULL;
-	if (!(curlst = (t_lst*)malloc(sizeof(t_lst))))
-		return (NULL);
-	if (content == NULL)
-	{
-		curlst->content = NULL;
-	}
-	else
-	{
-		if (!(curlst->content = malloc(sizeof(content))))
-			return (NULL);
-		curlst->content = ft_memmove(curlst->content, content, sizeof(content));
-	}
-	curlst->next = NULL;
-	return (curlst);
-}
-t_lst2		*create_lst2(void *content)
-{
-	t_lst2	*curlst;
-
-	curlst = NULL;
-	if (!(curlst = (t_lst2*)malloc(sizeof(t_lst2))))
-		return (NULL);
-	if (content == NULL)
-	{
-		curlst->content = NULL;
-	}
-	else
-	{
-		if (!(curlst->content = malloc(sizeof(content))))
-			return (NULL);
-		curlst->content = ft_memmove(curlst->content, content, sizeof(content));
-	}
-	curlst->nextl = NULL;
-	curlst->nextr = NULL;
-	return (curlst);
-}
-*/
