c617fb8c3b179e7ac7d16c3a74f4c8b068b9f9a0
Sulaiman YUSOF <syusof@e1r7p18.42.fr>
2016-12-22 16:33:15 +0100

    r

--- ft_maketree.c
+++ ft_maketree.c
@@ -6,18 +6,16 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/04 16:09:47 by syusof            #+#    #+#             */
-/*   Updated: 2016/12/21 14:19:13 by syusof           ###   ########.fr       */
+/*   Updated: 2016/12/22 16:32:14 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-
-t_node	*ft_maketree(t_node *maintree,char *strbegi)
+t_node	*ft_maketree(t_node *maintree, char *strbegi)
 {
 	t_node	*rtree;
-	
-	rtree = ft_copytree(maintree,strbegi);
 
+	rtree = ft_copytree(maintree, strbegi);
 	return (rtree);
 }
