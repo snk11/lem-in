32c728de34e643a35d4ad5b74c77c64b90d855c5
Sulaiman YUSOF <syusof@e1r7p18.42.fr>
2016-12-22 18:05:45 +0100

    r

--- function.c
+++ function.c
@@ -6,14 +6,11 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/31 18:32:04 by syusof            #+#    #+#             */
-/*   Updated: 2016/12/21 17:46:03 by syusof           ###   ########.fr       */
+/*   Updated: 2016/12/22 18:03:36 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "lem_in.h"
-
-
-#include <stdio.h>
+#include "lem_in.h"
 
 void		ft_lstop_add_down(t_lst **toplist, char *s)
 {
@@ -26,14 +23,14 @@ void		ft_lstop_add_down(t_lst **toplist, char *s)
 	lstmp = NULL;
 	lstmp2 = *toplist;
 	e = (t_op*)malloc(sizeof(t_op));
-	e->val = (char*)malloc(sizeof(char)*ft_strlen(s)+1);
+	e->val = (char*)malloc(sizeof(char)*ft_strlen(s) + 1);
 	while(*s)
 	{
 		e->val[i] = *s;
 		s++;
 		i++;
 	}
-		lstmp = create_lst(e);
+	lstmp = create_lst(e);
 	if (*toplist == NULL)
 		*toplist = lstmp;
 	else
@@ -52,16 +49,9 @@ t_lst		*create_lst(void *content)
 	if (!(curlst = (t_lst*)malloc(sizeof(t_lst))))
 		return (NULL);
 	if (content == NULL)
-	{
 		curlst->content = NULL;
-	}
 	else
-	{
-//		if (!(curlst->content = malloc(sizeof(content))))
-//			return (NULL);
-//		curlst->content = ft_memmove(curlst->content, content, sizeof(content));
 		curlst->content = ft_memmove2(content);
-	}
 	curlst->next = NULL;
 	return (curlst);
 }
@@ -72,7 +62,6 @@ t_node		*ft_remove_last_l(t_node **node1)
 
 	nodetmp = NULL;
 	nodetmp = *node1;
-
 	while(nodetmp && nodetmp->nextl && (nodetmp->nextl)->nextl)
 		nodetmp = nodetmp->nextl;
 	if(nodetmp)
@@ -82,7 +71,6 @@ t_node		*ft_remove_last_l(t_node **node1)
 
 t_node		*lst_add_downlrnew(t_node **node1,char *strbegi)
 {
-//	printf("newnormal\n");
 	t_node	*nodetmp;
 	t_node	*nodetmp2;
 	t_node	*nodetmp3;
@@ -95,31 +83,12 @@ t_node		*lst_add_downlrnew(t_node **node1,char *strbegi)
 	nodetmp2 = *node1;
 	nodetmp = NULL;
 	nodetmp4 = NULL;
-//	if(*node1 == NULL)
-	{
-		*node1 = lst_add_downl(node1,strbegi);
-		return (*node1);
-	}
-	/*
-	while(nodetmp2 && nodetmp2->nextl)
-		nodetmp2 = (nodetmp2)->nextl;
-//	nodetmp = ft_createnode(nodetmp2->name);
-//	nodetmp3 = nodetmp;
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp4 = NULL;
-	while(nodetmp2 && nodetmp2->nextl)
-		nodetmp2 = (nodetmp2)->nextl;
-	nodetmp = nodetmp2->nextl;
-	nodetmp3 = nodetmp;
 	*node1 = lst_add_downl(node1,strbegi);
 	return (*node1);
-	*/
 }
 
 t_room		*lst_add_downlrnew_room(t_room **node1,char *strbegi)
 {
-//	printf("newnormal\n");
 	t_room	*nodetmp;
 	t_room	*nodetmp2;
 	t_room	*nodetmp3;
@@ -132,176 +101,7 @@ t_room		*lst_add_downlrnew_room(t_room **node1,char *strbegi)
 	nodetmp2 = *node1;
 	nodetmp = NULL;
 	nodetmp4 = NULL;
-//	if(*node1 == NULL)
-	{
-		*node1 = lst_add_downl_room(node1,strbegi);
-		return (*node1);
-	}
-	/*
-	while(nodetmp2 && nodetmp2->nextl)
-		nodetmp2 = (nodetmp2)->nextl;
-//	nodetmp = ft_createnode(nodetmp2->name);
-//	nodetmp3 = nodetmp;
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp4 = NULL;
-	while(nodetmp2 && nodetmp2->nextl)
-		nodetmp2 = (nodetmp2)->nextl;
-	nodetmp = nodetmp2->nextl;
-	nodetmp3 = nodetmp;
 	*node1 = lst_add_downl_room(node1,strbegi);
 	return (*node1);
-	*/
 }
 
-t_room		*lst_add_downl_room(t_room **node1,char *s)
-{
-	t_room	*nodetmp;
-	t_room	*nodetmp2;
-
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp = ft_createnode_room(s);
-	if (!(nodetmp2))
-		return (nodetmp);
-	while ((nodetmp2)->nextl)
-		nodetmp2 = (nodetmp2)->nextl;
-	(nodetmp2)->nextl = nodetmp;
-	return (*node1);
-}
-
-t_room		*ft_createnode_room(char *s)
-{
-	t_room	*curlst;
-
-	curlst = NULL;
-	if (!(curlst = (t_room*)malloc(sizeof(t_room))))
-		return (NULL);
-	if (s == NULL)
-	{
-		curlst->name = NULL;
-	}
-	else
-	{
-		if (!(curlst->name = malloc(sizeof(char) * ft_strlen(s))))
-			return (NULL);
-		curlst->name = ft_memmove(curlst->name, s, sizeof(char) * ft_strlen(s));
-	}
-	curlst->client = 0;
-	curlst->nextl = NULL;
-	curlst->nextr = NULL;
-	return (curlst);
-}
-
-t_room		*lst_add_down_downr_room(t_room **node1,char *s)
-{
-	t_room	*nodetmp;
-	t_room	*nodetmp2;
-	int		ind1;
-
-	ind1 = 0;
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp = ft_createnode_room(s);
-	if (!(nodetmp2))
-		return (nodetmp);
-	while(nodetmp2 && nodetmp2->nextl)
-	{
-		nodetmp2 = (nodetmp2)->nextl;
-	}
-
-	while ((nodetmp2)->nextr)
-		nodetmp2 = (nodetmp2)->nextr;
-	(nodetmp2)->nextr = nodetmp;
-	return (*node1);
-}
-
-t_node		*ft_createnode(char *s)
-{
-	t_node	*curlst;
-
-	curlst = NULL;
-	if (!(curlst = (t_node*)malloc(sizeof(t_node))))
-		return (NULL);
-	if (s == NULL)
-	{
-		curlst->name = NULL;
-	}
-	else
-	{
-		if (!(curlst->name = malloc(sizeof(char) * ft_strlen(s))))
-			return (NULL);
-		curlst->name = ft_memmove(curlst->name, s, sizeof(char) * ft_strlen(s));
-	}
-	curlst->nextl = NULL;
-	curlst->nextr = NULL;
-	return (curlst);
-}
-
-t_node		*lst_add_downl(t_node **node1,char *s)
-{
-	t_node	*nodetmp;
-	t_node	*nodetmp2;
-
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp = ft_createnode(s);
-	if (!(nodetmp2))
-		return (nodetmp);
-	while ((nodetmp2)->nextl)
-		nodetmp2 = (nodetmp2)->nextl;
-	(nodetmp2)->nextl = nodetmp;
-	return (*node1);
-}
-
-t_node		*lst_add_downr(t_node **node1,t_node *noderef,char *s)
-{
-	t_node	*nodetmp;
-	t_node	*nodetmp2;
-	int		ind1;
-
-	ind1 = 0;
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp = ft_createnode(s);
-	if (!(nodetmp2))
-		return (nodetmp);
-	while(nodetmp2 && ind1 == 0)
-	{
-		if(ft_strcmp((nodetmp2)->name,(noderef)->name) == 0)
-			ind1 = 1;
-		else
-			nodetmp2 = (nodetmp2)->nextl;
-	}
-
-	if (nodetmp2)
-	{
-		while ((nodetmp2)->nextr)
-			nodetmp2 = (nodetmp2)->nextr;
-		(nodetmp2)->nextr = nodetmp;
-	}
-	return (*node1);
-}
-
-t_node		*lst_add_down_downr(t_node **node1,char *s)
-{
-	t_node	*nodetmp;
-	t_node	*nodetmp2;
-	int		ind1;
-
-	ind1 = 0;
-	nodetmp2 = *node1;
-	nodetmp = NULL;
-	nodetmp = ft_createnode(s);
-	if (!(nodetmp2))
-		return (nodetmp);
-	while(nodetmp2 && nodetmp2->nextl)
-	{
-		nodetmp2 = (nodetmp2)->nextl;
-	}
-
-	while ((nodetmp2)->nextr)
-		nodetmp2 = (nodetmp2)->nextr;
-	(nodetmp2)->nextr = nodetmp;
-	return (*node1);
-}
---
+++ function3.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   function3.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/22 18:04:07 by syusof            #+#    #+#             */
+/*   Updated: 2016/12/22 18:05:02 by syusof           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+t_node		*lst_add_downr(t_node **node1,t_node *noderef,char *s)
+{
+	t_node	*nodetmp;
+	t_node	*nodetmp2;
+	int		ind1;
+
+	ind1 = 0;
+	nodetmp2 = *node1;
+	nodetmp = NULL;
+	nodetmp = ft_createnode(s);
+	if (!(nodetmp2))
+		return (nodetmp);
+	while(nodetmp2 && ind1 == 0)
+	{
+		if(ft_strcmp((nodetmp2)->name,(noderef)->name) == 0)
+			ind1 = 1;
+		else
+			nodetmp2 = (nodetmp2)->nextl;
+	}
+	if (nodetmp2)
+	{
+		while ((nodetmp2)->nextr)
+			nodetmp2 = (nodetmp2)->nextr;
+		(nodetmp2)->nextr = nodetmp;
+	}
+	return (*node1);
+}
+
+t_node		*lst_add_down_downr(t_node **node1,char *s)
+{
+	t_node	*nodetmp;
+	t_node	*nodetmp2;
+	int		ind1;
+
+	ind1 = 0;
+	nodetmp2 = *node1;
+	nodetmp = NULL;
+	nodetmp = ft_createnode(s);
+	if (!(nodetmp2))
+		return (nodetmp);
+	while(nodetmp2 && nodetmp2->nextl)
+		nodetmp2 = (nodetmp2)->nextl;
+	while ((nodetmp2)->nextr)
+		nodetmp2 = (nodetmp2)->nextr;
+	(nodetmp2)->nextr = nodetmp;
+	return (*node1);
+}
