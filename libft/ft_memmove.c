fddaff17234daf12e09d205d79ead8d4a2e7539f
Sulaiman YUSOF <syusof@e1r7p18.42.fr>
2016-12-22 18:53:55 +0100

    r

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 14:52:52 by syusof            #+#    #+#             */
-/*   Updated: 2016/12/15 14:00:07 by syusof           ###   ########.fr       */
+/*   Updated: 2016/12/22 18:52:45 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,4 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	else
 		ft_memcpy(dest, src, n);
 	return ((void *)dest);
-	/*
-	size_t	i;
-	char	*s1;
-	char	*s2;
-	char	*temp;
-
-	temp = (char*)malloc(sizeof(char) * n);
-	s1 = (char*)dest;
-	s2 = (char*)src;
-	i = 0;
-	while (i < n)
-	{
-		temp[i] = s2[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		s1[i] = temp[i];
-		i++;
-	}
-	return (s1);
-	*/
 }
