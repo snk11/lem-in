6c63ca0ea9cd92019babb5e7a520af3b5dff2e5e
Sulaiman YUSOF <syusof@e3r5p3.42.fr>
2017-01-04 14:09:26 +0100

    r

--- srcs/ft_dfs.c
+++ srcs/ft_dfs.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/04 13:39:01 by syusof            #+#    #+#             */
-/*   Updated: 2017/01/04 14:06:59 by syusof           ###   ########.fr       */
+/*   Updated: 2017/01/04 14:08:48 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,41 +28,39 @@ void		ft_dfs(t_node *nodeprime, t_gene *gene1, char *strbegi,
 				&& ft_checkdouble_l(gene1->nodedata, nodetmp3->name))
 		{
 			(gene1->lv)++;
-			ft_dfs(nodeprime, gene1,nodetmp3->name,strend);
+			ft_dfs(nodeprime, gene1, nodetmp3->name, strend);
 		}
 		else
 			ft_dfs_p1(gene1, nodetmp3, nodetmp4);
 		nodetmp3 = nodetmp3->nextr;
 	}
 	if (gene1->lv < gene1->nbelem)
-			ft_dfs_p2(gene1, nodetmp4);
+		ft_dfs_p2(gene1, nodetmp4);
 	(gene1->lv)--;
 }
 
-
-
-
 void		ft_dfs_p1(t_gene *gene1, t_node *nodetmp3, t_node *nodetmp4)
 {
-			gene1->nodedata = lst_add_downl(&(gene1->nodedata), nodetmp3->name);
-			nodetmp4 = gene1->nodedata;
-			gene1->nodescreen = lst_add_downl(&(gene1->nodescreen), nodetmp4->name);
-			nodetmp4 = nodetmp4->nextl;
-			while (nodetmp4)
-			{
-				gene1->nodescreen = lst_add_down_downr(&(gene1->nodescreen), nodetmp4->name);
-				nodetmp4 = nodetmp4->nextl;
-			}
-			nodetmp4 = gene1->nodedata;
-			while (nodetmp4 && nodetmp4->nextl && (nodetmp4->nextl)->nextl)
-				nodetmp4 = nodetmp4->nextl;
-			nodetmp4->nextl = NULL;
+	gene1->nodedata = lst_add_downl(&(gene1->nodedata), nodetmp3->name);
+	nodetmp4 = gene1->nodedata;
+	gene1->nodescreen = lst_add_downl(&(gene1->nodescreen), nodetmp4->name);
+	nodetmp4 = nodetmp4->nextl;
+	while (nodetmp4)
+	{
+		gene1->nodescreen = lst_add_down_downr(&(gene1->nodescreen),
+				nodetmp4->name);
+		nodetmp4 = nodetmp4->nextl;
+	}
+	nodetmp4 = gene1->nodedata;
+	while (nodetmp4 && nodetmp4->nextl && (nodetmp4->nextl)->nextl)
+		nodetmp4 = nodetmp4->nextl;
+	nodetmp4->nextl = NULL;
 }
 
 void		ft_dfs_p2(t_gene *gene1, t_node *nodetmp4)
 {
-		nodetmp4 = gene1->nodedata;
-		while (nodetmp4 && nodetmp4->nextl && (nodetmp4->nextl)->nextl)
-			nodetmp4 = nodetmp4->nextl;
-		nodetmp4->nextl = NULL;
+	nodetmp4 = gene1->nodedata;
+	while (nodetmp4 && nodetmp4->nextl && (nodetmp4->nextl)->nextl)
+		nodetmp4 = nodetmp4->nextl;
+	nodetmp4->nextl = NULL;
 }
