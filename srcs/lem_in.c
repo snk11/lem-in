14d3bcc31fddc3de12231a4088ef3c49da60fc21
Sulaiman YUSOF <syusof@e3r2p1.42.fr>
2017-01-08 18:51:07 +0100

    r

--- srcs/lem_in.c
+++ srcs/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/29 14:39:42 by syusof            #+#    #+#             */
-/*   Updated: 2017/01/08 18:02:44 by syusof           ###   ########.fr       */
+/*   Updated: 2017/01/08 18:50:39 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ int			main(void)
 	}
 	dmain1.nodedummie = ft_generate2(dmain1.node2, dmain1.data1.strbegi,
 			dmain1.data1.strend);
-	ft_printsol(dmain1.nodedummie);
+//	ft_printsol(dmain1.nodedummie);
 	dmain1.nodedata = ft_screen(&(dmain1.nodedata), dmain1);
 	if (dmain1.nodedata == NULL)
 	{
 		write(1, "ERROR", 5);
 		return (0);
 	}
-//	ft_printmap(dmain1.map1, dmain1.data1.nbant);
+	ft_printmap(dmain1.map1, dmain1.data1.nbant);
 	dmain1.nodebest = ft_screenbest(&(dmain1.nodebest), dmain1.nodedata,
 			(dmain1.data1).strend);
 	if (dmain1.nodebest)
