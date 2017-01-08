859bd375ded0ccfcd7893f2382b716b23fbad2a9
Sulaiman YUSOF <syusof@e3r2p1.42.fr>
2017-01-08 16:12:10 +0100

    r

--- srcs/ft_dijkstra.c
+++ srcs/ft_dijkstra.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/05 05:42:09 by syusof            #+#    #+#             */
-/*   Updated: 2017/01/08 14:57:13 by syusof           ###   ########.fr       */
+/*   Updated: 2017/01/08 16:08:56 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,44 +56,54 @@ void		ft_dijkstra(t_node *nodeprime, t_gene *gene1, char *strbegi,
 		inddelfirst = 0;
 		if (nodetmp3)
 		{
-			if (ft_checkdouble_r(nodetmp5,nodetmp3->name) == 0)
+			if (ft_countelem_lvr(nodetmp5) > gene1->nbelem - 1)
 			{
 				ft_del_first(&nodetmp5);
 				inddelfirst = 1;
 			}
 			else
-				nodetmp5 = lst_add_downr(&nodetmp5, nodetmp5,nodetmp3->name);
+			{
+				//				nodetmp5 = lst_add_downr(&nodetmp5, nodetmp5,nodetmp3->name);
+				nodetmp6 = nodetmp5;
+				nodetmp5 = lst_add_downl(&nodetmp5, nodetmp6->name);
+				while (nodetmp6 && nodetmp6->nextr)
+				{
+					nodetmp6 = nodetmp6->nextr;
+					nodetmp5 = lst_add_down_downr(&nodetmp5, nodetmp6->name);
+				}
+				if (ft_checkdouble_r(nodetmp5,nodetmp3->name) == 0)
+					ft_del_last(&nodetmp5);
+				else
+					nodetmp5 = lst_add_down_downr(&nodetmp5, nodetmp3->name);
+				if (ft_strcmp(nodetmp3->name, strend) == 0)
+					indend = 1;
+				else if (ft_strcmp(nodetmp3->name, strbegi) == 0)
+					ft_del_last(&nodetmp5);
+			}
 		}
 		if (inddelfirst == 0)
 		{
-			if (ft_strcmp(nodetmp3->name, strend) == 0)
-				indend = 1;
-			if (ft_strcmp(nodetmp3->name, strbegi) == 0)
-				ft_del_first(&nodetmp5);
-			//		else
+			while (nodetmp3->nextr && indend == 0)
 			{
-				while (nodetmp3->nextr && indend == 0)
+				nodetmp6 = nodetmp7;
+				nodetmp3 = nodetmp3->nextr;
+				nodetmp5 = lst_add_downl(&nodetmp5, nodetmp6->name);
+				while (nodetmp6 && nodetmp6->nextr)
 				{
-					nodetmp6 = nodetmp7;
-					nodetmp3 = nodetmp3->nextr;
-					nodetmp5 = lst_add_downl(&nodetmp5, nodetmp6->name);
-					while (nodetmp6 && nodetmp6->nextr && nodetmp6->nextr->nextr)
-					{
-						nodetmp6 = nodetmp6->nextr;
-						if (ft_checkdouble_r(nodetmp5,nodetmp3->name) == 0)
-							ft_del_last(&nodetmp5);
-						else
-							nodetmp5 = lst_add_down_downr(&nodetmp5, nodetmp6->name);
-					}
-					nodetmp5 = lst_add_down_downr(&nodetmp5, nodetmp3->name);
-					if (ft_strcmp(nodetmp3->name, strend) == 0)
-						indend = 1;
-					else if (ft_strcmp(nodetmp3->name, strbegi) == 0)
-						ft_del_last(&nodetmp5);
+					nodetmp6 = nodetmp6->nextr;
+					nodetmp5 = lst_add_down_downr(&nodetmp5, nodetmp6->name);
 				}
+				if (ft_checkdouble_r(nodetmp5,nodetmp3->name) == 0)
+					ft_del_last(&nodetmp5);
+				else
+					nodetmp5 = lst_add_down_downr(&nodetmp5, nodetmp3->name);
+				if (ft_strcmp(nodetmp3->name, strend) == 0)
+					indend = 1;
+				else if (ft_strcmp(nodetmp3->name, strbegi) == 0)
+					ft_del_last(&nodetmp5);
 			}
+			ft_del_first(&nodetmp5);
 		}
-		(gene1->lv)++;
 		//			if(indend == 0)
 		//				ft_sortpath(&nodetmp5);
 		ft_printsol(nodetmp5);
