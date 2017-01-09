4eda6afcac010471b99efef05cf356ba44da125b
Sulaiman YUSOF <syusof@e3r2p1.42.fr>
2017-01-09 13:37:31 +0100

    r

--- srcs/ft_dijkstra.c
+++ srcs/ft_dijkstra.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/05 05:42:09 by syusof            #+#    #+#             */
-/*   Updated: 2017/01/08 18:40:42 by syusof           ###   ########.fr       */
+/*   Updated: 2017/01/09 13:36:50 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void		ft_dijkstra(t_node *nodeprime, t_gene *gene1, char *strbegi,
 	printf("--------\n");
 
 	//	while(nodetmp5 && indend == 0)
-	while(nodetmp5 && cnt < 4)
+	while(nodetmp5 && cnt < 2)
 	{
 		indend = 0;
 		ft_sortpath(&nodetmp5);
