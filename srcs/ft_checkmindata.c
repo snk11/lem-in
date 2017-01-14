0ffedb577e562244c0d697a002286e1dd635e952
Sulaiman YUSOF <syusof@e3r6p2.42.fr>
2017-01-14 13:07:16 +0100

    r

--- srcs/ft_checkmindata.c
+++ srcs/ft_checkmindata.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/14 18:53:01 by syusof            #+#    #+#             */
-/*   Updated: 2017/01/08 16:34:46 by syusof           ###   ########.fr       */
+/*   Updated: 2017/01/14 13:03:58 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ int	ft_checkmindata(t_data data1, t_node *nodetmp)
 {
 	if (data1.nbant == 0)
 		return (0);
-	if (data1.strbegi == NULL || (data1.strbegi && ft_cursref_first(nodetmp, data1.strbegi)->nextr == NULL))
+	if (data1.strbegi == NULL || (data1.strbegi
+				&& ft_cursref_first(nodetmp, data1.strbegi)->nextr == NULL))
 		return (0);
-	if (data1.strend == NULL || (data1.strend && ft_cursref_first(nodetmp, data1.strend)->nextr == NULL))
+	if (data1.strend == NULL || (data1.strend
+				&& ft_cursref_first(nodetmp, data1.strend)->nextr == NULL))
 		return (0);
 	return (1);
 }
--- srcs/ft_generate2.c
+++ srcs/ft_generate2.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/22 23:52:26 by syusof            #+#    #+#             */
-/*   Updated: 2017/01/08 17:37:13 by syusof           ###   ########.fr       */
+/*   Updated: 2017/01/14 13:06:33 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,11 @@ t_node	*ft_generate2(t_node *nodeprime, char *strbegi, char *strend)
 {
 	t_gene	gene1;
 
-//	ft_printsol(nodeprime);
 	gene1.nodedata = NULL;
 	gene1.nodescreen = NULL;
 	gene1.lv = 1;
 	gene1.nbelem = ft_countelem_lv1(nodeprime);
-//	ft_dfs(nodeprime, &gene1, strbegi, strend);
 	gene1.lv = 1;
 	ft_dijkstra(nodeprime, &gene1, strbegi, strend);
-//	ft_printsol(gene1.nodescreen);
 	return (gene1.nodescreen);
 }
